4b32d70f56a31015b23a7c3635f0ccdb67425816
felixkaugi <felixkaugi8@gmail.com>
2021-09-07 14:43:37 +0300

    index file with the route

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
+//importing express
 var express = require('express')
-var app = express();
-//set port
-var server = app.listen(7070, () =>{
-    app.get('/', (req, res)=>{
-        res.send('Hello World');
-    });
-});
+var app = express()
+//setting the server port'
+var server = app.listen(7070, ()=>{
+    //setting the route
+    app.get('/students/list', (req,res)=>{
+        //get response in the json formart
+        res.json(
+            {
+                Name: 'Kaugi',
+                id: 1,
+                Coures: 'computer science',
+                units:{
+                    cosc101: 'Database Management',
+                    cosc102: 'Fundamentals of Programming',
+                    cosc103: 'Digital Electronics',
+                    cosc104: 'Emergening Technology',
+                    cosc105: 'computing math',
+                    cosc106: 'object oriented programming'
+                },
+            }
+        )
+    })
+})
